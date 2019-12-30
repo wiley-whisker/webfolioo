7c58944a712e4a4cf3cb75395418700351a06e29
easyrun32 <38576697+easyrun32@users.noreply.github.com>
2019-12-29 20:28:20 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://stormadmin:123@cluster0-f3yzi.mongodb.net/team?retryWrites=true&w=majority",
+    "INSERT YOUR MONGODB URI ",
   secretOrKey: "secret"
 };
